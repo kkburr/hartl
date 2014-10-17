13561d2746ec3ff4c8ce536c0a2bae9d7fc00994
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-10-17 13:51:25 -0600

    Adds password field to User, bycrypt and 	additional validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validates :name, :email, presence: true
+	before_save { email.downcase! }
+	validates :name, presence: true, length: {maximum: 50}
+	validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }, 
+					presence: true, 
+					uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
