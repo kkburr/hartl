eef4f572dc3e26e33fddcaeff38efaf8dc1a79fa
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-10-17 13:14:26 -0600

    Adds bootstrap and styling Moves components of application file to partials Fixes routes for navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  resources :microposts
+  get 'help' => 'static_pages#help'
+  get 'about' =>'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
   resources :users
+end
+
+  # resources :microposts
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -62,4 +65,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
